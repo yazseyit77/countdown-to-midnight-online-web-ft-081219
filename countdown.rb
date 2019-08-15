d38ce2cd30da61b1dfde57bb5f1146e7a8827777
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-15 02:55:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(counter)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(counter)
+  counter = 10
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
