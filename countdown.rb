2e5d090e678486a98097118520eb50261c471653
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-15 02:59:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  counter = 10
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(counter)
+  counter = 10
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    sleep 5
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
