df8dbe7fbbf666a919b39cf7489cefa7b59a8839
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-15 02:54:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
-def countdown(n)
+def countdown(counter)
   counter = 10
   while counter > 0
-    puts "#{n} SECOND (S)!"
+    puts "#{counter} SECOND (S)!"
     counter -= 1
   end
   return "HAPPY NEW YEAR!"
