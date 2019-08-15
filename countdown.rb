a7f9914297300fe4f04f28f3494bb647073e03c8
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-15 02:53:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,10 @@
 #write your code here
-require "pry"
 
 def countdown(number)
   counter = number
   while counter > 0
     puts "#{number} SECOND (S)!"
     counter -= 1
-    binding.pry
   end
-
   return "HAPPY NEW YEAR!"
 end
